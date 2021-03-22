a84d8ab614bf485b70ffa8353be1a36361fa65c8
igor.dikan <igor.dikan@unitbean.com>
2021-03-22 22:39:43 +0300

    Подготовка проекта к разработке

---
+++ TestGit/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestGit
+//
+//  Created by Игорь Дикань on 22.03.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
